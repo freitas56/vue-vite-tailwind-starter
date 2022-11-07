57ba09e45354f5adc026e08760515b21f1c35789
72fcosta <inbox5372@gmail.com>
2022-11-07 15:35:57 -0300

    update LyAuth.vue, LyPublic.vue and PgHome.vue

---
+++ src/components/CpMain.vue
@@ -0,0 +1,7 @@
+<template>
+   <div class="bg-indigo-200">
+      <router-view />
+   </div>
+</template>
+
+<script setup></script>
--- src/layouts/LyPublic.vue
+++ src/layouts/LyPublic.vue
@@ -1,12 +1,11 @@
 <template>
    <cp-header />
-   <div class="bg-indigo-200">
-      <router-view />
-   </div>
+   <cp-main />
    <cp-footer />
 </template>
 
 <script setup>
 import CpHeader from "../components/CpHeader.vue"
+import CpMain from "../components/CpMain.vue"
 import CpFooter from "../components/CpFooter.vue"
 </script>
