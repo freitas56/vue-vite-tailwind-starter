65a032e08dd28dd90269b81294bf175da7ef4ef6
72fcosta <inbox5372@gmail.com>
2022-11-07 10:40:50 -0300

    update PgHome.vue

--- src/pages/PgHome.vue
+++ src/pages/PgHome.vue
@@ -1,6 +1,8 @@
 <template>
    <div class="bg-indigo-200">
-      <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">PgHome</div>
+      <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+         <p class="text-slate-900">PgHome</p>
+      </div>
    </div>
 </template>
 
