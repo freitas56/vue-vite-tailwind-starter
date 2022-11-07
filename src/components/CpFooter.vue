6ff51f3287fb0334dd76d8ef0a0210ed5affd2c7
72fcosta <inbox5372@gmail.com>
2022-11-07 10:38:27 -0300

    update CpFooter.vue, CpHeader.vue and PgHome.vue

--- src/components/CpFooter.vue
+++ src/components/CpFooter.vue
@@ -1,9 +1,9 @@
 <template>
-   <div class="bg-indigo-500 text-indigo-50">CpFooter</div>
+   <div class="bg-indigo-500">
+      <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+         <p class="text-indigo-50">CpFooter</p>
+      </div>
+   </div>
 </template>
 
-<script>
-export default {
-   name: "CpFooter",
-}
-</script>
+<script setup></script>
--- src/pages/PgHome.vue
+++ src/pages/PgHome.vue
@@ -1,9 +1,7 @@
 <template>
-   <div>PgHome</div>
+   <div class="bg-indigo-200">
+      <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">PgHome</div>
+   </div>
 </template>
 
-<script>
-export default {
-   name: "PgHome",
-}
-</script>
+<script setup></script>
