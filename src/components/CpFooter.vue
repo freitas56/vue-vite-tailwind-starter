77516eac3be3f85da8ac6ed3431d35084b617e70
72fcosta <inbox5372@gmail.com>
2022-11-07 10:43:25 -0300

    update CpFooter.vue and PgHome.vue

--- src/components/CpFooter.vue
+++ src/components/CpFooter.vue
@@ -1,5 +1,5 @@
 <template>
-   <div class="bg-indigo-500">
+   <div class="bg-indigo-500 py-2">
       <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
          <p class="text-indigo-50">CpFooter</p>
       </div>
--- src/pages/PgHome.vue
+++ src/pages/PgHome.vue
@@ -1,5 +1,5 @@
 <template>
-   <div class="bg-indigo-200">
+   <div class="bg-indigo-200 py-8">
       <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
          <p class="text-slate-900">PgHome</p>
       </div>
