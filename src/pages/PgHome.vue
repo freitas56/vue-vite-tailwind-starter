44b6454c98a30f496d3caa091a3c8546fa9199d9
72fcosta <inbox5372@gmail.com>
2022-11-09 17:21:12 -0300

    update PgHome.vue

--- src/pages/PgHome.vue
+++ src/pages/PgHome.vue
@@ -1,7 +1,9 @@
 <template>
    <div class="py-8">
       <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-         <p class="text-slate-400">Start="['Vue', 'Vite', 'Tailwind']"</p>
+         <p class="text-slate-400">
+            let templateStart=["Vue", "Vite", "Tailwind"]"
+         </p>
       </div>
    </div>
 </template>
