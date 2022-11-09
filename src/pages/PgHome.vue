f662129eeed6b49c1a6c801505ca98d576953e29
72fcosta <inbox5372@gmail.com>
2022-11-09 20:58:47 -0300

    update PgHome.vue

--- src/pages/PgHome.vue
+++ src/pages/PgHome.vue
@@ -2,7 +2,7 @@
    <div class="py-8">
       <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
          <p class="text-slate-400" style="color: #33ccff">
-            <span style="color: #b499ff">let </span>
+            <span style="color: #f07178">let </span>
             <span style="color: #a6accd"> templateStart = </span>
             <span>["</span>
             <span style="color: #b9e880">Vue</span>
