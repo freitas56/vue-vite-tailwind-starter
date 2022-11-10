0ad5b78d7595695403f1843838f68e7d6f584565
72fcosta <inbox5372@gmail.com>
2022-11-09 21:16:50 -0300

    update CpFooter.vue

--- src/components/CpFooter.vue
+++ src/components/CpFooter.vue
@@ -1,30 +1,28 @@
 <template>
-   <footer class="bg-slate-900 py-2">
-      <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-         <div class="flex">
+   <footer class="bg-slate-800 py-4">
+      <div class="flex items-center justify-between mx-auto max-w-7xl px-2">
+         <div>
+            <img class="block h-10 w-auto" src="/imgs/logo.png" />
+         </div>
+
+         <div class="flex items-center">
             <div>
-               <img class="block h-10 w-auto" src="/imgs/logo.png" />
+               <a class="rounded-full text-slate-400" href="#">
+                  <BsMoonStars class="h-5 w-auto" />
+               </a>
+
+               <!-- <a class="rounded-full text-slate-400" href="#">
+                  <BsSun class="h-5 w-auto" />
+               </a> -->
             </div>
 
-            <div>
-               <div>
-                  <a class="rounded-full text-slate-400" href="#">
-                     <BsMoonStars class="h-5 w-auto" />
-                  </a>
-
-                  <a class="rounded-full text-slate-400" href="#">
-                     <BsSun class="h-5 w-auto" />
-                  </a>
-               </div>
-
-               <div>
-                  <a
-                     class="rounded-full text-slate-400"
-                     href="https://github.com/72fcosta/vue-vite-tailwind-starter"
-                  >
-                     <BsGithub class="h-5 w-auto" />
-                  </a>
-               </div>
+            <div class="ml-3">
+               <a
+                  class="rounded-full text-slate-400"
+                  href="https://github.com/72fcosta/vue-vite-tailwind-starter"
+               >
+                  <BsGithub class="h-5 w-auto" />
+               </a>
             </div>
          </div>
       </div>
